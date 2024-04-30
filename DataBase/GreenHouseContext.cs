bd2e5b44ba002bf345f1111ad0a203a8ea6894f6
Javieruw <javier.uw@gmail.com>
2024-04-30 10:55:02 +0200

    database

---
+++ DataBase/GreenHouseContext.cs
@@ -0,0 +1,25 @@
+using Domain.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
+
+namespace Database
+{
+    public class GreenHouseContext : DbContext
+    {
+        public DbSet<GreenHouse> Greenhouses { get; set; }
+        public string DbPath { get; }
+
+        public GreenHouseContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = Path.Combine(path, "App.db");
+        }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source={DbPath}");
+        }
+    }
+}
---
+++ DataBase/Migrations/20240430085236_Initial.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Greenhouses",
+                columns: table => new
+                {
+                    GreenHouseId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    GreenHouseName = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Temperature = table.Column<double>(type: "REAL", nullable: true),
+                    LightIntensity = table.Column<double>(type: "REAL", nullable: true),
+                    Co2Levels = table.Column<double>(type: "REAL", nullable: true),
+                    Humidity = table.Column<double>(type: "REAL", nullable: true),
+                    IsWindowOpen = table.Column<bool>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Greenhouses", x => x.GreenHouseId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Greenhouses");
+        }
+    }
+}

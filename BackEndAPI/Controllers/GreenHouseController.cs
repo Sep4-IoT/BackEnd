32087018f8d549013d1f4c6d60b29be044a32f1b
Javieruw <javier.uw@gmail.com>
2024-05-17 10:47:39 +0200

    wow

--- BackEndAPI/Controllers/GreenHouseController.cs
+++ BackEndAPI/Controllers/GreenHouseController.cs
@@ -2,144 +2,106 @@ using Application.LogicInterfaces;
 using Domain.DTOs;
 using Domain.Model;
 using Microsoft.AspNetCore.Mvc;
-using System.Net.Http;
-using System.Threading.Tasks;
 
-namespace BackEnd.Controllers
+namespace BackEnd.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class  GreenHouseController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class GreenHouseController : ControllerBase
+    private readonly IGreenHouseLogic greenHouseLogic;
+
+    public GreenHouseController(IGreenHouseLogic greenHouseLogic)
     {
-        private readonly IGreenHouseLogic greenHouseLogic;
-        private readonly HttpClient httpClient;
-
-        public GreenHouseController(IGreenHouseLogic greenHouseLogic)
+        this.greenHouseLogic = greenHouseLogic;
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult<GreenHouse>> CreateAsync([FromBody] GreenHouseCreationDTO dto)
+    {
+        try
         {
-            this.greenHouseLogic = greenHouseLogic;
-            this.httpClient = new HttpClient { BaseAddress = new Uri("http://localhost:6000/") };
+            GreenHouse greenHouse = await greenHouseLogic.CreateAsync(dto);
+            return Created($"/GreenHouse/{greenHouse.GreenHouseName}", greenHouse);
         }
-
-        [HttpPost]
-        public async Task<ActionResult<GreenHouse>> CreateAsync([FromBody] GreenHouseCreationDTO dto)
+        catch (Exception e)
         {
-            try
-            {
-                GreenHouse greenHouse = await greenHouseLogic.CreateAsync(dto);
-                return Created($"/GreenHouse/{greenHouse.GreenHouseName}", greenHouse);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                return StatusCode(500, e.Message);
-            }
-        }
-
-        [HttpGet("{greenHouseId}")]
-        public async Task<ActionResult<GreenHouse>> GetAsync(int greenHouseId)
-        {
-            try
-            {
-                GreenHouse greenHouse = await greenHouseLogic.GetByIdAsync(greenHouseId);
-
-                // Get the current status from the IOT server
-                string iotStatus = await FetchIOTStatusFromServerAsync(greenHouseId);
-                greenHouse.IsWindowOpen = iotStatus == "Open";
-
-                return Ok(greenHouse);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                return StatusCode(500, e.Message);
-            }
-        }
-
-        [HttpGet("owner/{ownerId}")]
-        public async Task<ActionResult<IEnumerable<GreenHouse>>> GetByOwnerIdAsync(int ownerId)
-        {
-            try
-            {
-                IEnumerable<GreenHouse> greenHouses = await greenHouseLogic.GetByOwnerIdAsync(ownerId);
-                return Ok(greenHouses);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                return StatusCode(500, e.Message);
-            }
-        }
-
-        [HttpPatch("{greenHouseId}")]
-        public async Task<ActionResult<UpdateGreenHouseDTO>> UpdateAsync(int greenHouseId, string? greenHouseName, string? description, bool? isWindowOpen)
-        {
-            try
-            {
-                var existingGreenHouse = await greenHouseLogic.GetByIdAsync(greenHouseId);
-
-                greenHouseName ??= existingGreenHouse.GreenHouseName;
-                description ??= existingGreenHouse.Description;
-
-                if (isWindowOpen.HasValue && isWindowOpen != existingGreenHouse.IsWindowOpen)
-                {
-                    if (isWindowOpen.Value)
-                    {
-                        await OpenWindowAsync(greenHouseId);
-                    }
-                    else
-                    {
-                        await CloseWindowAsync(greenHouseId);
-                    }
-                }
-                else
-                {
-                    isWindowOpen = existingGreenHouse.IsWindowOpen;
-                }
-
-                UpdateGreenHouseDTO updated = new UpdateGreenHouseDTO(greenHouseId, greenHouseName, description, isWindowOpen);
-                var greenHouse = await greenHouseLogic.UpdateAsync(updated);
-                return Ok(greenHouse);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                return StatusCode(500, e.Message);
-            }
-        }
-
-        [HttpGet("IOT/{id}/getStatus")]
-        public async Task<IActionResult> GetIOTStatusAsync(int id)
-        {
-            try
-            {
-                string status = await FetchIOTStatusFromServerAsync(id);
-                return Ok(status);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                return StatusCode(500, e.Message);
-            }
-        }
-
-        private async Task<string> FetchIOTStatusFromServerAsync(int greenHouseId)
-        {
-            var response = await httpClient.GetAsync($"GreenHouse/IOT/{greenHouseId}/getStatus?clientId=1");
-            response.EnsureSuccessStatusCode();
-            string status = await response.Content.ReadAsStringAsync();
-            return status;
-        }
-
-        private async Task OpenWindowAsync(int greenHouseId)
-        {
-            var response = await httpClient.PostAsync($"GreenHouse/IOT/{greenHouseId}/openWindow?clientId=1", null);
-            response.EnsureSuccessStatusCode();
-        }
-
-        private async Task CloseWindowAsync(int greenHouseId)
-        {
-            var response = await httpClient.PostAsync($"GreenHouse/IOT/{greenHouseId}/closeWindow?clientId=1", null);
-            response.EnsureSuccessStatusCode();
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
         }
     }
+    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<GreenHouse>>> GetAsync([FromQuery] int? greenHouseId)
+    { 
+        try
+        {
+            SearchGreenHouseDTO parameters = new(greenHouseId);
+            IEnumerable<GreenHouse> greenHouses = await greenHouseLogic.GetAsync(parameters);
+            return Ok(greenHouses);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
+    
+    [HttpGet("owner/{ownerId}")]
+    public async Task<ActionResult<IEnumerable<GreenHouse>>> GetByOwnerIdAsync(int ownerId)
+    { 
+        try
+        {
+            IEnumerable<GreenHouse> greenHouses = await greenHouseLogic.GetByOwnerIdAsync(ownerId);
+            return Ok(greenHouses);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
+    
+    
+    
+    [HttpPatch("{greenHouseId}")]
+    public async Task<ActionResult<UpdateGreenHouseDTO>> UpdateAsync(int greenHouseId,
+        string? greenHouseName, string? description, bool? isWindowOpen)
+    {
+        try
+        {
+            var existingGreenHouse = await greenHouseLogic.GetByIdAsync(greenHouseId);
+            
+            greenHouseName ??= existingGreenHouse.GreenHouseName;
+            description ??= existingGreenHouse.Description;
+            isWindowOpen ??= existingGreenHouse.IsWindowOpen;
+
+            UpdateGreenHouseDTO updated =
+                new UpdateGreenHouseDTO(greenHouseId, greenHouseName, description, isWindowOpen);
+        
+            var greenHouse = await greenHouseLogic.UpdateAsync(updated);
+            return Ok(greenHouse);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return StatusCode(500, e.Message);
+        }
+    }
+    
+    [HttpGet("greenHouse/{greenHouseId}")]
+    public async Task<ActionResult<IEnumerable<GreenHouse>>> GetByIdAsync(int greenHouseId)
+    { 
+        try
+        {
+            GreenHouse? greenHouse = await greenHouseLogic.GetByIdAsync(greenHouseId);
+            return Ok(greenHouse);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
+    
 }
